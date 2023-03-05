e0c066de838fcc292cff32779ab71de01360ae67
kimdohyun <kdh2646@naver.com>
2023-03-05 22:24:35 +0900

    😣

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,6 +16,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Row(
         children: const [
           LeftSide(),
--- lib/widgets/left_side.dart
+++ lib/widgets/left_side.dart
@@ -12,162 +12,161 @@ class LeftSide extends StatefulWidget {
 class _LeftSideState extends State<LeftSide> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 2,
-      child: Container(
-        color: Colors.white,
-        child: ScrollConfiguration(
-          behavior: ScrollConfiguration.of(context).copyWith(dragDevices: {
-            PointerDeviceKind.mouse,
-            PointerDeviceKind.touch,
-          }, scrollbars: false),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            physics: const BouncingScrollPhysics(),
-            child: Column(
-              children: [
-                Column(
-                  children: [
-                    Image.asset("assets/icons/KOING Logo_GRD.png",width: 50,),
-                    Image.asset("assets/icons/Admin.png",scale: 4,),
-                  ],
-                ),
-SizedBox(height: 500,),
-                Column(children: [
-                  Container(
-                    height: 30,
-                    // color: Colors.red,
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.home,
-                          color: Colors.black,
-                          size: 28,
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          "Feed",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    height: 30,
-                    // color: Colors.red,
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.search_outlined,
-                          color: Colors.black54,
-                          size: 28,
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          "Explore",
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    height: 30,
-                    // color: Colors.red,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                    ),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.video_library_outlined,
-                          color: Colors.black54,
-                          size: 28,
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          "Reels",
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    height: 30,
-                    // color: Colors.red,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                    ),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.settings_outlined,
-                          color: Colors.black54,
-                          size: 28,
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          "Settings",
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  Container(
-                    height: 30,
-                    // color: Colors.red,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                    ),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.logout_outlined,
-                          color: Colors.black54,
-                          size: 28,
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          "Logout",
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                ],)
-
-
-              ],
-            ),
+    return Container(
+      width: 200,
+      color: Colors.white,
+      child: Column(
+        children: [
+          const SizedBox(height: 80),
+          Column(
+            children: [
+              Image.asset("assets/icons/KOING Logo_GRD.png",width: 70,),
+              Image.asset("assets/icons/Admin.png",scale: 4,),
+            ],
           ),
-        ),
+SizedBox(height: 50,),
+          Column(children: [
+            Container(
+              height: 30,
+              // color: Colors.red,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 28,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "대시보드",
+                    style: TextStyle(
+                      color: Color(0xfff63c6e),
+                      fontSize: 18,
+                      fontFamily: "Pretendard",
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 30,
+              // color: Colors.red,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.search_outlined,
+                    color: Colors.black54,
+                    size: 28,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "유저 관리", style: TextStyle(
+                      color: Color(0xfff63c6e),
+                fontSize: 18,
+                fontFamily: "Pretendard",
+                fontWeight: FontWeight.w600,
+              ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 30,
+              // color: Colors.red,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.video_library_outlined,
+                    color: Colors.black54,
+                    size: 28,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "탐험 관리",
+                    style: TextStyle(
+                      color: Color(0xfff63c6e),
+                      fontSize: 18,
+                      fontFamily: "Pretendard",
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 30,
+              // color: Colors.red,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.settings_outlined,
+                    color: Colors.black54,
+                    size: 28,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "채팅 관리",
+                    style: TextStyle(
+                      color: Color(0xfff63c6e),
+                      fontSize: 18,
+                      fontFamily: "Pretendard",
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 600),
+            Container(
+              height: 30,
+              // color: Colors.red,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.logout_outlined,
+                    color: Colors.black54,
+                    size: 28,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "Logout",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],)
+
+
+        ],
       ),
     );
   }
